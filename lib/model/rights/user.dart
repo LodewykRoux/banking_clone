030657c84d46bd34f312f306fed9d39b10608b09
lodewyk <lodewyk@thinkninjas.co.za>
2023-01-23 06:30:51 +0200

    WIP: Adding firebase with users

---
+++ lib/model/rights/user.dart
@@ -0,0 +1,166 @@
+
+class User {
+  static final _collection =
+      FirebaseFirestore.instance.collection('users').withConverter(
+            fromFirestore: _fromFirestore,
+            toFirestore: _toFirestore,
+          );
+
+  final String uid;
+  final String fullName;
+  final String email;
+  final bool emailVerified;
+  final bool isSuperUser;
+  final bool isActive;
+  final Timestamp? createdOn;
+  final String? createdByName;
+  final DocumentReference? createdBy;
+  final Timestamp? updatedOn;
+  final String? updatedByName;
+  final DocumentReference? updatedBy;
+  final DocumentReference ref;
+
+  const User({
+    required this.uid,
+    required this.fullName,
+    required this.email,
+    required this.emailVerified,
+    required this.isSuperUser,
+    this.markets,
+    this.types,
+    required this.isActive,
+    this.createdOn,
+    this.createdByName,
+    this.createdBy,
+    this.updatedOn,
+    this.updatedByName,
+    this.updatedBy,
+    required this.ref,
+  });
+
+  User copyWith({
+    final String? fullName,
+    final bool? isActive,
+    final bool? isSuperUser,
+    final Timestamp? createdOn,
+    final String? createdByName,
+    final DocumentReference? createdBy,
+    final Timestamp? updatedOn,
+    final String? updatedByName,
+    final DocumentReference? updatedBy,
+    final List<DocumentReference>? markets,
+    final List<DocumentReference>? types,
+  }) {
+    return User(
+      uid: uid,
+      fullName: fullName ?? this.fullName,
+      email: email,
+      emailVerified: emailVerified,
+      isSuperUser: isSuperUser ?? this.isSuperUser,
+      markets: markets ?? this.markets,
+      types: types ?? this.types,
+      isActive: isActive ?? this.isActive,
+      createdOn: createdOn,
+      createdByName: createdByName,
+      createdBy: createdBy,
+      updatedOn: updatedOn ?? this.updatedOn,
+      updatedByName: updatedByName ?? this.updatedByName,
+      updatedBy: updatedBy ?? this.updatedBy,
+      ref: ref,
+    );
+  }
+
+  factory User.fromSnapshot(DocumentSnapshot<Map<String, dynamic>?> snapshot) {
+    final data = snapshot.data();
+
+    if (data == null) throw CustomException('Not implemented');
+
+    return User(
+      createdOn: data['createdOn'],
+      fullName: data['fullName'],
+      email: data['email'],
+      uid: data['uid'],
+      emailVerified: data['emailVerified'],
+      isSuperUser: data['isSuperUser'] ?? false,
+      markets:
+          data['markets'] != null ? List.unmodifiable(data['markets']) : null,
+      types: data['types'] != null ? List.unmodifiable(data['types']) : null,
+      isActive: data['isActive'],
+      createdByName: data['createdByName'],
+      createdBy: data['createdBy'],
+      updatedOn: data['updatedOn'],
+      updatedByName: data['updatedByName'],
+      updatedBy: data['updatedBy'],
+      ref: snapshot.reference,
+    );
+  }
+
+  Map<String, dynamic> toFirestore() {
+    return {
+      'uid': uid,
+      'fullName': fullName,
+      'email': email,
+      'emailVerified': emailVerified,
+      'isSuperUser': isSuperUser,
+      'markets': markets,
+      'types': types,
+      'isActive': isActive,
+      'createdOn': createdOn,
+      'createdByName': createdByName,
+      'createdBy': createdBy,
+      'updatedOn': updatedOn,
+      'updatedByName': updatedByName,
+      'updatedBy': updatedBy,
+    };
+  }
+
+  static User _fromFirestore(
+    DocumentSnapshot<Map<String, dynamic>?> snapshot,
+    SnapshotOptions? options,
+  ) =>
+      User.fromSnapshot(snapshot);
+
+  static Map<String, Object?> _toFirestore(
+    User user,
+    SetOptions? options,
+  ) =>
+      user.toFirestore();
+
+  static Stream<DocumentSnapshot<User?>> snapshot(String uid) =>
+      _collection.doc(uid).snapshots();
+
+  static Stream<QuerySnapshot<User>> snapshots() => _collection.snapshots();
+
+  Future<DocumentSnapshot<Object?>> save() async {
+    DocumentReference<Object?>? resultRef = ref;
+    await _collection.doc(ref.id).set(this);
+
+    return await resultRef.get();
+  }
+
+  @override
+  int get hashCode {
+    return uid.hashCode ^
+        fullName.hashCode ^
+        email.hashCode ^
+        emailVerified.hashCode ^
+        isSuperUser.hashCode ^
+        Object.hashAllUnordered(markets ?? []) ^
+        Object.hashAllUnordered(types ?? []) ^
+        isActive.hashCode ^
+        (createdOn?.hashCode ?? 0) ^
+        createdByName.hashCode ^
+        createdBy.hashCode ^
+        (updatedOn?.hashCode ?? 0) ^
+        updatedByName.hashCode ^
+        updatedBy.hashCode ^
+        ref.hashCode;
+  }
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is User &&
+          runtimeType == other.runtimeType &&
+          hashCode == other.hashCode;
+}
