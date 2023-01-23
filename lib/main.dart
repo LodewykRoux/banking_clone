80ba9fae360b5c8b8925e09158912d99a8ee7ffe
lodewyk <lodewyk@thinkninjas.co.za>
2023-01-23 06:21:31 +0200

    Added firebase to project

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:banking_clone/screens/auth/login.dart';
 import 'package:banking_clone/service/navigation_service.dart';
 import 'package:banking_clone/service/scaffold_messenger_service.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
