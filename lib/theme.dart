74fbc335ded5cbe8130eb5262f72876afda020ab
lodewyk <lodewyk@thinkninjas.co.za>
2023-02-08 06:56:23 +0200

    Setting up theme

---
+++ lib/theme.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class BankingCloneTheme {
+  static ThemeData get lightTheme => BankingCloneTheme()._lightTheme;
+  static ThemeData get darkTheme => BankingCloneTheme()._darkTheme;
+
+  final ThemeData _lightTheme = ThemeData(
+    
+  );
+
+  final ThemeData _darkTheme = ThemeData();
+}
